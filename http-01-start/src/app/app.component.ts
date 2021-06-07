3440b7d5fd97c5bbf0196b7a3bc2ee1f3f2b7649
robertlongmendez <robertlongmendez@gmail.com>
2021-06-07 13:45:23 -0500

    Update app.component.ts
    and updated maps

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -11,18 +12,45 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    // console.log(postData);
+    this.http.post(
+      'https://ng-complete-guide-3747e-default-rtdb.firebaseio.com/post.json',
+      postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+      this.http.get('https://ng-complete-guide-3747e-default-rtdb.firebaseio.com/post.json')
+      .pipe(map(responseData => {
+        const postArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+             postArray.push({ ...responseData[key], id: key});
+          }
+       }
+       return postArray;
+      })
+      )
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
